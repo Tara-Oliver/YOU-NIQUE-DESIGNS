1d9bb52151d663e1cea3970af65b03ac5032b8cd
Tara-Oliver <tara_m_oliver@yahoo.com>
2025-02-07 19:54:38 -0500

    added portfolio logo

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -18,14 +18,14 @@ export function init(contentWrapper) {
 	header.append(contactLinksDiv, playBtnDiv);
 	// header.append(contactLinksDiv);
 
-	//create link for gitHub page
+	//////create link for gitHub page/////
 	const gitlink = document.createElement("a"); // create the link
 	gitlink.setAttribute(
 		"href",
 		"https://github.com/tarao1185/YOU-NIQUE-DESIGNS.git"
 	);
 
-	//create gitHub logo image with class of logo
+	//////create gitHub logo image with class of logo//////
 	const github_logo = document.createElement("img");
 	github_logo.src = "assets/images/github.svg";
 	// github_logo.src = "assets/images/waves.svg";
@@ -35,22 +35,40 @@ export function init(contentWrapper) {
 	//add the gitHub logo image to the gitHub link
 	gitlink.append(github_logo);
 
-	//create link for LinkedIn page
+	//////create link for LinkedIn page//////
 	const linkedin_link = document.createElement("a");
 	linkedin_link.setAttribute("href", "https://www.linkedin.com/in/tara-oliver");
 
-	//create LinkedIn logo image with class of logo
+	//////create LinkedIn logo image with class of logo//////
 	const linkedin_logo = document.createElement("img");
 	linkedin_logo.src = "assets/images/linkedin.svg";
 	linkedin_link.setAttribute("class", "logo");
 	linkedin_link.setAttribute("alt", "LinkedIn logo");
 	linkedin_link.setAttribute("aria-label", "LinkedIn logo and link");
 
+	//create link for portfolio page
+	const initial_link = document.createElement("a"); // create the link
+	gitlink.setAttribute("href", "https://tara-oliver.vercel.app/");
+
+	//create initial logo image with class of logo
+	const initial_logo = document.createElement("img");
+	initial_logo.src = "assets/images/initial.svg";
+	// github_logo.src = "assets/images/waves.svg";
+	initial_link.setAttribute("class", "logo");
+	initial_link.setAttribute("alt", "personal logo");
+	initial_link.setAttribute("aria-label", "personal logo and link");
+
+	//add the gitHub logo image to the gitHub link
+	gitlink.append(github_logo);
+
 	//add the LinkedIn logo image to the LinkedIn link
 	linkedin_link.append(linkedin_logo);
 
+	//add the initial logo image to the initial link
+	initial_link.append(initial_logo);
+
 	// add LinkedIn and gitHub logos to the contactLinks div(left side of header btm)
-	contactLinksDiv.append(gitlink, linkedin_link);
+	contactLinksDiv.append(gitlink, linkedin_link, initial_link);
 
 	//create how to play button with classes btn and playbtn
 	const howToPlayBtn = document.createElement("button");
